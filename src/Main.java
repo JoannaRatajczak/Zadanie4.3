02d841a92e2fbde333684319ed24e236604f20d4
JoannaRatajczak <joanna.jr.ratajczak@gmail.com>
2019-05-12 20:32:52 +0200

    Pola figur wersja z metodami w klasie. Edycja pól zgodnie z poleceniem zadania.

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,15 @@ public class Main {
         Circle circle = new Circle(5.5);
         Triangle triangle = new Triangle(4.5, 8, 6.6);
 
-        double sqrArea = square.squareArea();
+        double sqrArea=square.squareArea();
+        double restPerimiter=rectangle.rectPerimeter();
+        double circleArea=circle.circleArea();
+        double trianglePerimiter=triangle.trianglePerimeter();
 
-
-        System.out.println("Square area: " + square.squareArea());
-        System.out.println("Rectangle perimiter: " + rectangle.rectPerimeter());
-        System.out.println("Circle area: " + circle.circleArea());
-        System.out.println("Triangle perimiter: " + triangle.trianglePerimeter());
+        System.out.println("Square area: " + sqrArea);
+        System.out.println("Rectangle perimiter: " + restPerimiter);
+        System.out.println("Circle area: " + circleArea);
+        System.out.println("Triangle perimiter: " + trianglePerimiter);
 
 
     }
